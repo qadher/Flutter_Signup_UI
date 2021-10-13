b3dfb3a662a67a33257169640c227c8673c6ba1f
qadher <qadher7@gmail.com>
2021-10-13 13:04:27 +0530

    nothing

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -19,7 +19,7 @@ class HomePage extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(height: 52.h),
+              SizedBox(height: 26.h),
               Text(
                 "Sign Up to Mastermind",
                 style: TextStyle(
@@ -28,27 +28,26 @@ class HomePage extends StatelessWidget {
                   color: darkTextColor,
                 ),
               ),
-              SizedBox(height: 4.h),
+              SizedBox(height: 2.h),
               const LoginText(
                 greytext: 'Already have an account? ',
                 bluetext: 'Login',
               ),
-              SizedBox(height: 24.h),
+              SizedBox(height: 12.h),
               const GetTextField(hint: "Full Name"),
-              SizedBox(height: 16.h),
+              SizedBox(height: 8.h),
               const GetTextField(hint: "Email"),
-              SizedBox(height: 16.h),
+              SizedBox(height: 8.h),
               const GetTextField(hint: "Password"),
-              SizedBox(height: 16.h),
+              SizedBox(height: 8.h),
               const GetTextField(hint: "Confirm Password"),
-              SizedBox(height: 16.h),
+              SizedBox(height: 8.h),
               const CreateAccountButton(),
-              SizedBox(height: 16.h),
+              SizedBox(height: 8.h),
               Row(
                 children: [
-                  const Expanded(
-                    flex: 1,child: Divider()),
-                  SizedBox(width: 16.w),
+                  const Expanded(flex: 1, child: Divider()),
+                  SizedBox(width: 8.w),
                   Text(
                     "or Sign via",
                     style: TextStyle(
@@ -57,17 +56,18 @@ class HomePage extends StatelessWidget {
                       color: lightTextColor,
                     ),
                   ),
-                  SizedBox(width: 16.w),
+                  SizedBox(width: 8.w),
                   const Expanded(child: Divider()),
                 ],
               ),
-              SizedBox(height: 16.h),
+              SizedBox(height: 8.h),
               const GoogleSignin(),
-              SizedBox(height: 16.h),
+              SizedBox(height: 8.h),
               const LoginText(
-                greytext: 'By signing up tp Masterminds uou agree to our ',
+                greytext: 'By signing up tp Masterminds you agree to our ',
                 bluetext: 'terms and conditions',
               ),
+              SizedBox(height: 8.h),
             ],
           ),
         ),
